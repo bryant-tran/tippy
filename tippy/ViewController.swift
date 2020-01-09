95bb5a281445fb0970e8da7f3d65ddd8556bac2c
Bryant Tran <bryant.n.tran@sjsu.edu>
2020-01-08 20:03:44 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Bryant Tran on 1/8/20.
+//  Copyright © 2020 Bryant Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
